3343c0f548ec582cc9aaf2550a77e6d1cba25f7e
Uggla <uggla@free.fr>
2024-04-04 11:39:41 +0200

    fix: Remove formater and dap
    - This will be handled by community plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -23,7 +23,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
+        -- "prettier",
         -- "stylua",
         -- add more arguments for adding more null-ls sources
       })
@@ -35,7 +35,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "codelldb",
+        -- "codelldb",
         -- add more arguments for adding more debuggers
       })
     end,
