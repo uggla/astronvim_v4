34b781b238b03b5fe5e02e3c685b28ab0f59a424
René Ribaud <rribaud@redhat.com>
2024-05-31 09:55:15 +0200

    feat: Add blame-nvim community plugin
    - Better git blame, with history navigation.
      https://github.com/FabijanZulj/blame.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -13,5 +13,6 @@ return {
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.mini-splitjoin" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.git.blame-nvim" },
   -- import/override with your plugins folder
 }
